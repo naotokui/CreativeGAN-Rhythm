d26c1b5121bd987d9554c71d0512493ea7345f8c
Nao Tokui <tokui@qosmo.jp>
2020-03-03 07:04:10 +0900

    worked with no fill dataset

--- rhythm_can/analysis.py
+++ rhythm_can/analysis.py
@@ -11,7 +11,7 @@ def edit_distance(onsets1, onsets2):
         distance += dldistance(c1, c2)
     return distance
 
-def get_similarity_matrix(matrices_onsets, matrices_genres, GENRES, matrices_target,  target_genre):
+def get_similarity_matrix(matrices_onsets, matrices_genres, GENRES, matrices_target = None,  target_genre = None):
     # threshold to ignore too weak onsets
     THRESHOLD = 0.15
 
@@ -34,22 +34,25 @@ def get_similarity_matrix(matrices_onsets, matrices_genres, GENRES, matrices_tar
             distances[genre1].append(dist)     
 
     # calculate distances within target patterns
-    distances_inter = []
-    for i in tqdm_notebook(range(matrices_target.shape[0])):
-        onsets1 = matrices_target[i]
-        onsets1[onsets1 < THRESHOLD] = 0 # ignore too weak onsets!
+    if matrices_target is not None:
+        distances_inter = []
+        for i in tqdm_notebook(range(matrices_target.shape[0])):
+            onsets1 = matrices_target[i]
+            onsets1[onsets1 < THRESHOLD] = 0 # ignore too weak onsets!
 
-        for j,onsets2 in enumerate(matrices_target):
-            if i != j:
-                onsets2[onsets2 < THRESHOLD] = 0 # ignore weak onsets!
-                dist = edit_distance(onsets1, onsets2)
-                distances_inter.append(dist)  
+            for j,onsets2 in enumerate(matrices_target):
+                if i != j:
+                    onsets2[onsets2 < THRESHOLD] = 0 # ignore weak onsets!
+                    dist = edit_distance(onsets1, onsets2)
+                    distances_inter.append(dist)  
                 
     # return result
     all_dists = [np.array(l).mean() for l in distances]
-    all_dists.append(np.array(distances_inter).mean())
+    if matrices_target is not None:
+        all_dists.append(np.array(distances_inter).mean())
 
     genre_names = list(GENRES)
-    genre_names.append(target_genre)
+    if matrices_target is not None:
+        genre_names.append(target_genre)
 
     return all_dists, genre_names
